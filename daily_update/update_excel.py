5f7ac02b1e154313776dab657e72408b659fe468
kwy07311 <kwy07311@gmail.com>
2026-07-29 12:58:19 +0900

    Update update_excel.py

--- daily_update/update_excel.py
+++ daily_update/update_excel.py
@@ -1,57 +1,29 @@
 import pandas as pd
-import os
+
+from pykrx_range import get_range_data
+
+STOCK_FILE = "input/mydata.xlsx"
 
 
-FILE="data/range_history.xlsx"
+def update_excel():
 
+    date, range_dict = get_range_data()
 
-def update_excel(result):
+    print(date, "데이터 조회 완료")
 
+    df = pd.read_excel(STOCK_FILE)
 
-    os.makedirs(
-        "data",
-        exist_ok=True
-    )
+    # 이미 컬럼이 있으면 덮어쓰기
+    if date not in df.columns:
+        df[date] = None
 
+    for idx, row in df.iterrows():
 
-    if os.path.exists(FILE):
+        code = str(row["name"]).split("_")[-1]
 
-        df=pd.read_excel(FILE)
+        if code in range_dict:
+            df.at[idx, date] = range_dict[code]
 
-    else:
+    df.to_excel(STOCK_FILE, index=False)
 
-        df=pd.DataFrame(
-            columns=["name"]
-        )
-
-
-
-    new=pd.DataFrame(result)
-
-
-
-    today=new.columns[-1]
-
-
-
-    # 기존 날짜 삭제 후 재작성
-
-    if today in df.columns:
-
-        df.drop(
-            columns=[today],
-            inplace=True
-        )
-
-
-    df=df.merge(
-        new,
-        on="name",
-        how="outer"
-    )
-
-
-    df.to_excel(
-        FILE,
-        index=False
-    )
+    print("엑셀 저장 완료")
