8dbff3628bcf0e8a7700f63fa71843cd263bf205
kwy07311 <kwy07311@gmail.com>
2026-07-30 06:07:55 +0900

    Update update_excel.py

--- daily_update/update_excel.py
+++ daily_update/update_excel.py
@@ -1,70 +1,32 @@
 import pandas as pd
-
 from pykrx_range import get_range_data
-
+from logger import log
 
 STOCK_FILE = "input/mydata.xlsx"
 
-
 def extract_code(value):
-
     value = str(value)
-
-    # 마지막 _ 뒤 숫자 추출
     code = value.split("_")[-1]
-
     return code.zfill(6)
 
-
-
 def update_excel():
-
     df = pd.read_excel(STOCK_FILE)
-
-
     codes = []
-
     for value in df["name"]:
-
         code = extract_code(value)
-
         codes.append(code)
 
-
-    print(
-        f"조회 종목 수 : {len(codes)}"
-    )
-
+    log(f"조회 종목 수 : {len(codes)}")
 
     date, range_data = get_range_data(codes)
-
-
-    print(
-        f"{date} 데이터 업데이트"
-    )
-
+    log(f"{date} 데이터 업데이트")
 
     if date not in df.columns:
-
         df[date] = None
 
-
     for idx, code in enumerate(codes):
-
         if code in range_data:
+            df.at[idx, date] = range_data[code]
 
-            df.at[
-                idx,
-                date
-            ] = range_data[code]
-
-
-    df.to_excel(
-        STOCK_FILE,
-        index=False
-    )
-
-
-    print(
-        "엑셀 업데이트 완료"
-    )
+    df.to_excel(STOCK_FILE, index=False)
+    log("엑셀 업데이트 완료")
