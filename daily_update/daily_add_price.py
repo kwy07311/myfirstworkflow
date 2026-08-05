09bde4269ecdac2945ba06f10b412a679928f3e2
kwy07311 <kwy07311@gmail.com>
2026-08-05 09:28:59 +0900

    Update daily_add_price.py

--- daily_update/daily_add_price.py
+++ daily_update/daily_add_price.py
@@ -1,6 +1,12 @@
+import sys
 from update_excel import update_excel
 
 
 if __name__ == "__main__":
 
-    update_excel()
+    target_date = None
+
+    if len(sys.argv) > 1:
+        target_date = sys.argv[1]
+
+    update_excel(target_date)
